21ab9e2176e832875ebd0b2af3f1aa2ca3314cdd
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-08 17:04:39 +0200

    Fix broken code in Feladat3.java

--- src/hu/petrik/javabevezeto/Feladat3.java
+++ src/hu/petrik/javabevezeto/Feladat3.java
@@ -8,16 +8,16 @@ public class Feladat3 {
         double num = -1;
         while (num < 0)
         {
-            System.out.println("Adjon meg egy pozitív valós számot: ");
+            System.out.print("Adjon meg egy pozitív valós számot: ");
             num = sc.nextDouble();
         }
         double lowerNum = Math.floor(num);
         double higherNum = Math.ceil(num);
         double closerToNum = Math.round(num);
-        String[] numToString = String.valueOf(num).split(",");
+        double decimal = num-lowerNum;
 
-        System.out.printf("A megadott szám a %l és a %h egész számok között van, és ezek közül a %.0c számhoz van közelebb.", lowerNum, higherNum, closerToNum);
-        System.out.printf("A szám egész része: %i", numToString[0]);
-        System.out.printf("A szám törtrésze: %d", numToString[1]);
+        System.out.printf("A megadott szám a %.0f és a %.0f egész számok között van, és ezek közül a %.0f számhoz van közelebb.", lowerNum, higherNum, closerToNum);
+        System.out.printf("\nA szám egész része: %.0f", lowerNum);
+        System.out.printf("\nA szám törtrésze: %f", decimal);
     }
 }
