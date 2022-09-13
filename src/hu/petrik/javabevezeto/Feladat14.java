c88a61650140efdebab9c2157cff4972b4af5336
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-13 15:29:25 +0200

    Fix if condition never being true

--- src/hu/petrik/javabevezeto/Feladat14.java
+++ src/hu/petrik/javabevezeto/Feladat14.java
@@ -9,7 +9,7 @@ public class Feladat14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(in);
 
-        out.print("Adj meg egy szöveget");
+        out.print("Adj meg egy szöveget: ");
         String text = sc.nextLine();
         String choice = "asd";
         out.println("Nagy betűssé alakítás (n)");
@@ -18,32 +18,33 @@ public class Feladat14 {
         out.println("Összehasonlítás egy másik stringgel (c)");
         out.println("A string egy részének kiírása (i)");
         out.println("Kilépés (q)");
-        out.print("Válassz egy opciót a zárójelben lévő karakter beírásával: ");
-        while (choice.toLowerCase() != "q")
+        while (!choice.toLowerCase().equals("q"))
         {
-            choice = sc.nextLine().toLowerCase();
-            if(choice == "n")
+            out.print("Válassz egy opciót a zárójelben lévő karakter beírásával: ");
+            choice = sc.nextLine();
+            if(choice.toLowerCase().equals("n"))
             {
                 out.println(text.toUpperCase());
             }
-            else if(choice == "k")
+            else if(choice.toLowerCase().equals("k"))
             {
                 out.println(text.toLowerCase());
             }
-            else if(choice == "h")
+            else if(choice.toLowerCase().equals("h"))
             {
                 out.println(text.length());
             }
-            else if(choice == "c")
+            else if(choice.toLowerCase().equals("c"))
             {
+                out.print("Adja meg a második szöveget: ");
                 String secondText = sc.nextLine();
-                out.printf("%s - Hossza: %d\n%s - Hossza: %d", text, text.length(), secondText, secondText.length());
+                out.printf("%s - Hossza: %d\n%s - Hossza: %d\n", text, text.length(), secondText, secondText.length());
             }
-            else if(choice == "i")
+            else if(choice.toLowerCase().equals("i"))
             {
-                out.print("Add meg, hogy hanyadik betűtől akarod kiíratni a szöveget");
+                out.print("Add meg, hogy hanyadik betűtől akarod kiíratni a szöveget: ");
                 int a = sc.nextInt();
-                out.print("Add meg, hogy hanyadik betűig akarod kiíratni a szöveget");
+                out.print("Add meg, hogy hanyadik betűig akarod kiíratni a szöveget: ");
                 int b = sc.nextInt();
                 out.println(text.substring(a-1, b-1));
             }
